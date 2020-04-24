d2029bc736feb3ef5bc0c3d7a633325b062f3e55
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@jda.com>
2020-04-24 22:42:33 +0200

    Initial commit

---
+++ Formula/di-csv2xml.rb
@@ -0,0 +1,33 @@
+class DiCsv2xml < Formula
+  desc "Convert CSV to Blue Yonder Supply & Demand API compatible XML"
+  version "v2.0.0"
+  url "https://github.com/blue-yonder/di-csv2xml/archive/#{version}.tar.gz"
+  sha256 "46b6653a472036f3c77f428261106c1e14d880ab335fd7701bc4a79924f965ad"
+  homepage "https://github.com/blue-yonder/di-csv2xml"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--bin", "di-csv2xml", "--locked", "--release"
+    bin.install "target/release/di-csv2xml"
+  end
+
+  test do
+    (testpath/"test.csv").write <<~EOS
+      A,B,C,D
+      1,2,3,4
+    EOS
+    (testpath/"test.xml").write <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <Category>
+      \t<Record>
+      \t\t<A>1</A>
+      \t\t<B>2</B>
+      \t\t<C>3</C>
+      \t\t<D>4</D>
+      \t</Record>
+      </Category>
+    EOS
+    assert_equal (testpath/"test.xml").read.strip, shell_output("#{bin}/di-csv2xml --category Category --input test.csv").strip
+  end
+end
